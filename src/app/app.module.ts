b4e83dbba2a828746c0346115e6bd82051aaf019
MaratEs <marik744056@gmail.com>
2023-03-08 12:40:56 +0300

    ang

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { ContainerComponent } from './Container/container.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
